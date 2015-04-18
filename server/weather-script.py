ba788780f648a9a08a49aad0ce67f2517d481074
Alistair Bill <alistair.bill@gmail.com>
2015-04-18 20:11:25 +0100

    Use with OWM rather than US service.
    You need pyowm now.

--- server/weather-script.py
+++ server/weather-script.py
@@ -1,71 +1,110 @@
 #!/usr/bin/python2
 
 # Kindle Weather Display
-# Matthew Petroff (http://mpetroff.net/)
-# September 2012
-
-from xml.dom import minidom
+# Matthew Petroff (http://www.mpetroff.net/)
+# September 2012, modified by Alistair Bill April 2015
+ 
 import datetime
 import codecs
-try:
-    # Python 3
-    from urllib.request import urlopen
-except ImportError:
-    # Python 2
-    from urllib2 import urlopen
+import pyowm
 
-#
-# Geographic location
-#
-
-latitude = 39.3286
-longitude = -76.6169
-
-
-
-#
-# Download and parse weather data
-#
-
-# Fetch data (change lat and lon to desired location)
-weather_xml = urlopen('http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=' + str(latitude) + '&lon=' + str(longitude) + '&format=24+hourly&numDays=4&Unit=e').read()
-dom = minidom.parseString(weather_xml)
-
-# Parse temperatures
-xml_temperatures = dom.getElementsByTagName('temperature')
-highs = [None]*4
-lows = [None]*4
-for item in xml_temperatures:
-    if item.getAttribute('type') == 'maximum':
-        values = item.getElementsByTagName('value')
-        for i in range(len(values)):
-            highs[i] = int(values[i].firstChild.nodeValue)
-    if item.getAttribute('type') == 'minimum':
-        values = item.getElementsByTagName('value')
-        for i in range(len(values)):
-            lows[i] = int(values[i].firstChild.nodeValue)
-
+# Variable declaration
+owm = pyowm.OWM('fbf212320d9257d0f997610f1da48735')
+location = 'Bristol,UK'
+icons = []
+icons_parse = []
+highs = []
+lows = []
+dates = []
+wf = owm.daily_forecast(location, limit=4)
+forecast = wf.get_forecast()
+for weather in forecast:
 # Parse icons
-xml_icons = dom.getElementsByTagName('icon-link')
-icons = [None]*4
-for i in range(len(xml_icons)):
-    icons[i] = xml_icons[i].firstChild.nodeValue.split('/')[-1].split('.')[0].rstrip('0123456789')
-
+    icons.append(weather.get_weather_code())
+# Parse temperature highs
+    highs.append(round(weather.get_temperature(unit='celsius')['max'], 1))
+# Parse temperature lows
+    lows.append(round(weather.get_temperature(unit='celsius')['min'], 1))
 # Parse dates
-xml_day_one = dom.getElementsByTagName('start-valid-time')[0].firstChild.nodeValue[0:10]
-day_one = datetime.datetime.strptime(xml_day_one, '%Y-%m-%d')
+    dates.append(weather.get_reference_time('iso')[:-12])
 
 
+# Parse date
+day_one = datetime.datetime.strptime(str(dates[0]), '%Y-%m-%d')
 
-#
-# Preprocess SVG
-#
+# Change icons from OWM format to the format the svg wants them in. This is
+# really ugly code. I couldn't think of a better way to do it though, so for
+# now it'll just stay like this. I'm sorry :'(
+
+for item in icons:
+    if item == 800:
+        icons_parse.append('skc')
+    elif item == 801:
+        icons_parse.append('few')
+    elif item == 802:
+        icons_parse.append('sct')
+    elif item == (803 or 771 or 762):
+        icons_parse.append('bkn')
+    
+    elif item == (300 or 310 or 520):
+        icons_parse.append('hi_shwrs')
+    elif item == (301 or 302 or 311 or 312 or 313 or 314 or 321 or 521 or 522 or 531):
+        icons_parse.append('shra')
+    elif item == (200 or 210 or 221 or 230 or 231):
+        icons_parse.append('scttsra')
+    elif item == (201 or 202 or 211 or 212 or 232 or 960 or 961):
+        icons_parse.append('tsra')
+    
+    elif item == 804:
+        icons_parse.append('ovc')
+    elif item == (500 or 501):
+        icons_parse.append('ra')
+    elif item == (502 or 503 or 504):
+        icons_parse.append('hi_ra')
+    elif item == (905 or 958 or 959):
+        icons_parse.append('wind')
+    elif item == 904:
+        icons_parse.append('hot')
+        
+    elif item == 701:
+        icons_parse.append('mist')
+    elif item == 721:
+        icons_parse.append('sctfg')
+    elif item == 741:
+        icons_parse.append('fg')
+    elif item == 711:
+        icons_parse.append('smoke')
+    elif item == 761:
+        icons_parse.append('du')
+    elif item == (781 or 900):
+        icons_parse.append('nsurtsra')
+    
+    elif item == 903:
+        icons_parse.append('cold')
+    elif item == (600 or 601):
+        icons_parse.append('sn')
+    elif item == 602:
+        icons_parse.append('blizzard')
+    elif item == (611 or 612):
+        icons_parse.append('ip')
+    
+    elif item == (511 or 906):
+        icons_parse.append('fzra')
+    elif item == (620 or 621 or 622):
+        icons_parse.append('mix')
+    elif item == (615 or 616):
+        icons_parse.append('rasn')
+    else:
+        icons_parse.append('sct')
+        print ('Not recognised code')
+        
+print (icons_parse)
 
 # Open SVG to process
 output = codecs.open('weather-script-preprocess.svg', 'r', encoding='utf-8').read()
 
 # Insert icons and temperatures
-output = output.replace('ICON_ONE',icons[0]).replace('ICON_TWO',icons[1]).replace('ICON_THREE',icons[2]).replace('ICON_FOUR',icons[3])
+output = output.replace('ICON_ONE',icons_parse[0]).replace('ICON_TWO',icons_parse[1]).replace('ICON_THREE',icons_parse[2]).replace('ICON_FOUR',icons_parse[3])
 output = output.replace('HIGH_ONE',str(highs[0])).replace('HIGH_TWO',str(highs[1])).replace('HIGH_THREE',str(highs[2])).replace('HIGH_FOUR',str(highs[3]))
 output = output.replace('LOW_ONE',str(lows[0])).replace('LOW_TWO',str(lows[1])).replace('LOW_THREE',str(lows[2])).replace('LOW_FOUR',str(lows[3]))
 
